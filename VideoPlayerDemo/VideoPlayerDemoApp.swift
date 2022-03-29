fc8ec702ea9b6ba8ff64d41026680f36a45092f1
Vijay Lama <vijaylama2010@gmail.com>
2022-03-29 13:10:44 -0400

    Initial Commit

---
+++ VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by Vijay Lama on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
